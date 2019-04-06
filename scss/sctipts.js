8c09d050cf614da6912ef334b0d840b7d63d849f
VBelsky <vitaly.sx.belsky@gmail.com>
2019-04-06 20:04:21 +0300

    Выполнены правки согласно коментариям

--- scss/sctipts.js
+++ scss/sctipts.js
@@ -1,26 +1,35 @@
-var link = document.querySelector('.main-product__button');
-var modal = document.querySelector('.modal');
-var overlay = document.querySelector('.modal__overlay');
+/*Подгрузка svg спрайта*/
+
+var link = document.querySelector('link[rel=import]');
+var content = link.import.querySelector('.svg-catalog');
+document.body.appendChild(content.cloneNode(true));
+
+
+/*Модальное окно на главной странице*/
+
+var link = document.querySelector('.js-main-product__button');
+var modal = document.querySelector('.js-modal');
+var overlay = document.querySelector('.js-modal__overlay');
 
 link.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modal.classList.add('modal--show');
-  overlay.classList.add('modal__overlay--show');
+  modal.classList.add('js-modal--show');
+  overlay.classList.add('js-modal__overlay--show');
 });
 
 overlay.addEventListener('click', function (evt) {
   evt.preventDefault();
-  modal.classList.remove('modal--show');
-  overlay.classList.remove('modal__overlay--show');
+  modal.classList.remove('js-modal--show');
+  overlay.classList.remove('js-modal__overlay--show');
 });
 
 window.addEventListener('keydown', function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
 
-    if (modal.classList.contains('modal--show')) {
-      modal.classList.remove('modal--show');
-      overlay.classList.remove('modal__overlay--show');
+    if (modal.classList.contains('js-modal--show')) {
+      modal.classList.remove('js-modal--show');
+      overlay.classList.remove('js-modal__overlay--show');
     }
   }
 });
